725537544e96c2d55ea6d25668a80a7416faa9b8
Ismael Martinez <ismalfmp@gmail.com>
2025-02-16 23:44:17 -0500

    Logo changes according to theme

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,12 +3,16 @@ import Button from 'primevue/button';
 import { ref, onMounted } from "vue";
 const darkIcon = "pi pi-moon";
 const lightIcon = "pi pi-sun";
+const darkIconQilay = new URL("../assets/logo2.svg", import.meta.url);
+const lightIconQilay = new URL("../assets/logoleaf-light.svg", import.meta.url);
 const overrideThemeKey = "theme-override";
 
 let themeIcon = ref("");
+let themeQilay = ref(null);
 
 let toggleTheme = () => {
     themeIcon.value = document.documentElement.classList.toggle('dark-mode') ? lightIcon : darkIcon;
+    themeQilay.value = themeIcon.value === lightIcon ? darkIconQilay : lightIconQilay;
     saveOverrideTheme();
 }
 
@@ -19,6 +23,7 @@ let saveOverrideTheme = () => {
 onMounted(() => {
     const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
     themeIcon.value = prefersDark ? lightIcon : darkIcon;
+    themeQilay.value = prefersDark ? darkIconQilay : lightIconQilay;
     if(prefersDark && localStorage.getItem(overrideThemeKey) === 'light') {
         toggleTheme();
     }
@@ -29,7 +34,8 @@ onMounted(() => {
 
     </div>
     <div class="font-roboto sticky top-0 left-0 z-100 px-2 py-4 flex items-center h-[10vh] justify-between w-full">
-        <img src="../assets/logo2.svg" class="w-10" alt="">
+        <img :src="themeQilay" class="w-10" alt="">
+
         <div class="flex items-center justify-between w-[100px]">
             <Button rounded variant="text" @click="toggleTheme">
                 <span :class="themeIcon"></span>
